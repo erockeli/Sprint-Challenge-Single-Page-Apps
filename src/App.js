df6fb1ac30c71bcdb2df875bdf5ee115b5f70786
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-18 10:16:49 -0500

    Added Routes to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Header from "./components/Header.js";
 export default function App() {
   return (
     <main>
-      <Header />
+      <Header/>
+      <Route exact path="/" component={WelcomePage}/>
+      <Route exact path="/characters" component={CharacterList}/>
+      <Route exact path="/search" component={SearchForm}/>
+      <Route exact path="/location" component={LocationsList}/>
     </main>
   );
 }
