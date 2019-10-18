d752525bbf94ab522a288a06d1c4ce241165a44e
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-18 11:02:27 -0500

    Info from API connected to location and being mapped will create locationcard.js after this

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <div>
+    <h2>{props.name}</h2>
+    <p>Status: {props.status}</p>
+    <p>Gender: {props.gender}</p>
+    <p>Species: {props.species}</p>
+    <img src={props.image} />
+    </div>
+    )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,34 @@
 import React, { useEffect, useState } from "react";
 
-export default function LocationsList() {}
+export default function LocationsList() {
+
+
+    const [location, setLocation]= useState([])
+
+    useEffect(() => {
+        // TODO: Add API Request here - must run in `useEffect`
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+        axios.get("https://rickandmortyapi.com/api/location/").then(res => {
+          // setCharacters(res.data.results);
+          console.log(res.data.results);
+          setCharacters(res.data.results);
+        }).catch(e => console.log(e));
+      }, []);
+
+      return (
+        <section>
+          {location.map(location => 
+            
+              <LocationCard
+                id={location.id}
+                name={location.name}
+                type={location.type}
+                dimension={location.dimension}
+                residents={location.residents}
+              />   
+          )
+          }
+        </section>
+      );
+    
+}
